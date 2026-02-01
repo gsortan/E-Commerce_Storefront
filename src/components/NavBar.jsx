d3a09820c730b5ff81dfe898dc7c8fa5e7b0f4cf
Gordon Tan <gordonsortan@gmail.com>
2026-02-01 18:50:00 -0500

    Updated search bar with cleaner breakpoint responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,16 +18,19 @@ export default async function NavBar({ user }) {
           />
         </Link>
 
-        <div className=" p-4 hidden sm:block">
+        <div
+          className="
+          col-span-3 row-start-2 pt-2
+          sm:col-start-2 sm:col-span-1 sm:row-start-1 sm:p-4
+          
+        "
+        >
           <SearchBar />
         </div>
 
         <div className="flex items-center gap-4 col-start-3 row-start-1">
           <CartButton />
           <Profile user={user} />
-        </div>
-        <div className=" col-span-3 row-start-2 sm:col-start-2 sm:col-span-1 sm:row-start-1 pt-2 sm:hidden">
-          <SearchBar />
         </div>
       </div>
     </>
